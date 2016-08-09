722c3f7cb9efcf64ce0f59cc19730b7442c58afc
UMA SHANKAR SAHOO <uss.sahoo@gmail.com>
2016-08-09 16:03:18 +0530

    Added a comment

--- HelloGit/src/com/hello/git/HelloGit.java
+++ HelloGit/src/com/hello/git/HelloGit.java
@@ -1,7 +1,7 @@
 package com.hello.git;
 
 public class HelloGit {
-	// My first commit
+	// My first commit success
 	public static void main(String[] args) {
 		System.out.println("Hi there!");
 	}
